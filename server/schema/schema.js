d674a885cda3f389fbfe4157987007d7152e1c37
Sahil Saini <76941329+sahilsaini110@users.noreply.github.com>
2023-01-27 08:39:37 +0000

    schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require('graphql');
+const lo = require('lodash');
 
-const {GraphQLObjectType, GraphqlQLString}=graphql;
+const {GraphQLObjectType, GraphqlQLString, GraphQLSchema}=graphql;
+
+// dummy data
+
+var books = [
+    {name:'hello', genre:'kick',id:'1'},
+    {name:'hello2', genre:'kick1',id:'2'},
+    {name:'hello3', genre:'kick2',id:'3'}
+]
 
 const BookType = new GraphQLObjectType({
     name:'Book',
@@ -9,4 +18,23 @@ const BookType = new GraphQLObjectType({
         name:{type: GraphqlQLString},
         genre:{type: GraphqlQLString}
     })
+});
+
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        book: {
+            type:BookType,
+            args:{id:{type:GraphqlQLString}},
+            resolve(parent,args){
+                // code to get data from db / other resources
+               return lo.find(books,{id:args.id});
+            }
+        }
+    }
+});
+
+module.export = new GraphQLSchema({
+    query: RootQuery
 });
